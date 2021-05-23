1deeb68654498dd9dd730046d6dcaa99b1647427
prerna bhatra <Prerna-Dev-at-319929695488>
2021-05-23 12:47:15 +0530

    singup

--- App.js
+++ App.js
@@ -1,13 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View ,StatusBar, TextInput } from 'react-native';
+import { Button } from 'react-native-paper';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+      <Text style={{marginLeft:18,fontSize:35,color:"grey"}}>Welcome to </Text>
+        <Text style={styles.row}>React Native CRUD</Text>
+        <Text style={{marginLeft:18,fontSize:30,color:"grey"}}>Create Account</Text>
+        {/* <Text >Email</Text> */}
+        <TextInput
+         style={styles.input}
+        placeholder=" Email"
+
+      />
+      <TextInput
+            style={styles.input}
+            placeholder="Password"
+
+          />
+          <button >
+            Sign Up
+          </button>
+      </View>
   );
 }
 
@@ -17,5 +33,16 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  row: {
+    padding: 4,
+    borderBottomColor: "blue",
+    borderBottomWidth: 10
+  },
+  input: {
+    paddingLeft:10,
+    height: 30,
+    margin: 12,
+    borderWidth: 1,
   },
 });
